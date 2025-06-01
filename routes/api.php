96d205043da1a42212a985a8efbbf934856ecadb
inventoawlad <awlad@invento.com.bd>
2025-06-01 13:23:42 +0600

    refactor(routes): remove sanctum middleware from blog categories route
    The auth middleware was unnecessarily restricting access to the categories endpoint which should be publicly accessible. This change aligns with the intended public API design.

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Invento\Blog\Controllers\BlogApiController;
 
 // API routes
 Route::prefix('api/v1')->group(function () {
-        Route::middleware('auth:sanctum')->group(function (){
+        Route::group(function (){
 
             Route::get('/blogs/categories', [BlogApiController::class, 'categories'])->name('api.categories.index');
 
