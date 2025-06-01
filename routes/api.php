f68bfbb2efc69b917d2c1b93d79dccd89a288653
inventoawlad <awlad@invento.com.bd>
2025-06-01 13:59:37 +0600

    refactor(routes): clean up api routes structure and imports
    - Remove unnecessary nested route group
    - Add proper imports for Route facade and BlogApiController
    - Improve code readability by reducing indentation levels

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route; // It's good practice to import Route facade
+use App\Controllers\BlogApiController; // Assuming this is the correct namespace
 
-use Invento\Blog\Controllers\BlogApiController;
-
-// API routes
 Route::prefix('api/v1')->group(function () {
-        Route::group(function (){
-
-            Route::get('/blogs/categories', [BlogApiController::class, 'categories'])->name('api.categories.index');
-
-            Route::get('/blogs', [BlogApiController::class, 'index'])->name('api.blogs.index');
-            Route::get('/blogs/{slug}', [BlogApiController::class, 'show'])->name('api.blogs.show');
-    });
+    Route::get('/blogs/categories', [BlogApiController::class, 'categories'])->name('api.categories.index');
+    Route::get('/blogs', [BlogApiController::class, 'index'])->name('api.blogs.index');
+    Route::get('/blogs/{slug}', [BlogApiController::class, 'show'])->name('api.blogs.show');
 });
